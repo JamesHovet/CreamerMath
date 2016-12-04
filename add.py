d3cc9998ef23a7fe2d8eae20f2976a62dcf9d6fe
jrhovet <jrhovet@gmail.com>
2016-12-04 10:19:14 -0500

    first commit

---
+++ add.py
@@ -0,0 +1,7 @@
+s=0
+print('Please a smaller than b')
+a=int(input('Enter a number:'))
+b=int(input('Enter a number:'))
+for i in range(a,b+1):
+        s=s+i
+print('The sum of the integers between',a,'and',b,'is',s)
