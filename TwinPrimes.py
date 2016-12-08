4501afb98fd33d7a75368a23dab1fc50f4ef04f0
jrhovet <jrhovet@gmail.com>
2016-12-08 11:10:27 -0500

    added sqrt upper limit to TwinPrimes.py

--- TwinPrimes.py
+++ TwinPrimes.py
@@ -1,5 +1,8 @@
+import math
+
 def isPrime(n):
-    for i in range(2,n): # go thru all numbers 1 to just under the number
+    topLimit = math.ceil(math.sqrt(n))
+    for i in range(2,topLimit): # go thru all numbers 1 to just under the number
         if n % i == 0: # if that number is a factor of the number we are getting from the user
             return False # break out of the function because that number is not a prime
     return True # if we go thru all the numbers up to the imput number and there is not a factor, than this line of code says that that number is prime
